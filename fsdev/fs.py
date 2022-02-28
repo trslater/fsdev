b5fe889da8cfe90260ac51826d6b5de366df14a0
Tristan Slater <hello@trslater.ca>
2022-02-28 12:45:33 -0800

    Implement basic building

--- fsdev/fs.py
+++ fsdev/fs.py
@@ -1,5 +1,6 @@
 """Filesystem handling"""
 
+import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
 
@@ -13,4 +14,26 @@ def read(root, path):
 
 
 def build(root):
-    raise NotImplemented()
+    build_dir = Path("build")
+
+    directories = [root]
+
+    while directories:
+        directory = directories.pop()
+        
+        with os.scandir(directory) as scanner:
+            for entry in scanner:
+                if entry.is_dir():
+                    directories.append(entry)
+                
+                elif entry.is_file():
+                    if entry.name[0] == "_":
+                        continue
+
+                    rel_path = entry.path.replace(f"{root}/", "")
+                    output_path = build_dir/rel_path
+
+                    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+                    with output_path.open("w+") as f:
+                        f.write(read(root, rel_path))
