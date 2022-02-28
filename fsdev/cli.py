4a1b9a25b5226104b4856828b17dfed9c2f9c416
Tristan Slater <hello@trslater.ca>
2022-02-27 22:45:08 -0800

    Switch to using built-in HTTP server and Jinja

--- fsdev/cli.py
+++ fsdev/cli.py
@@ -1,54 +1,18 @@
-""".. include:: README.md"""
+from .server import Server
 
-__docformat__ = "restructuredtext"
-
-# Standard library
-import os
-from pathlib import Path
-import sys
-
-# Dependencies
-import flask
-from flask import Flask
-
-MIMETYPES = {
-    "css": "text/css",
-    "js": "text/javascript",
-    "png": "image/png",
-    "jpg": "image/jpeg",
-    "gif": "image/gif",
-    "svg": "image/svg+xml"}
-
-APP = Flask(__name__,
-            template_folder=f"{os.getcwd()}/templates",
-            static_folder=f"{os.getcwd()}/static")
+HOST = "localhost"
+PORT = 8000
 
 
-@APP.get(f"/<path:path>")
-@APP.get(f"/")
-def doc(path=""):
-    """Route templates or static files within CWD"""
+def run():
+    print(f"Server started at http://{HOST}:{PORT}\n\n"
+          "  Ctrl+C to exit\n")
 
-    # Strip trailing slash
-    path = path.strip("/")
-
-    # Ignore private templates
-    if path and path[0] == "_":
-        flask.abort(403, "Private template.")
+    server = Server(HOST, PORT)
 
     try:
-        extension = path.split("/")[-1].split(".")[1]
+        server.serve_forever()
 
-    # If no extension...
-    except IndexError:
-        # Treat as directory
-        return flask.render_template(f"{path}/index.html")
-
-    # If there is an extension...
-    else:
-        if extension == "html":
-            return flask.render_template(f"{path}")
-
-        elif extension in MIMETYPES.keys():
-            return flask.send_from_directory(APP.static_folder, path,
-                                             mimetype=MIMETYPES[extension])
+    except KeyboardInterrupt:
+        server.server_close()
+        print("Server stopped.")
---
+++ fsdev/server.py
@@ -0,0 +1,47 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import mimetypes
+import socket
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape, Template
+
+TEMPLATE_MIMETYPES = ("text/html",)
+
+
+class Server(HTTPServer):
+    """A simple extension of the built-in `HTTPServer` to always use the same
+    handler."""
+
+    def __init__(self, host: str, port: int) -> None:
+        super().__init__((host, port), Handler)
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Assume everything works
+        self.send_response(200)
+
+        # Handle indexes
+        self.path += "index.html" if self.path[-1] == "/" else ""
+
+        # Discover and set appropriate MIME type
+        mimetype, _ = mimetypes.guess_type(self.path)
+        self.send_header("Content-type", mimetype)
+        self.end_headers()
+
+        if mimetype in TEMPLATE_MIMETYPES:
+            # Get contents from Jinja template
+            env = Environment(
+                loader=FileSystemLoader("templates"),
+                autoescape=select_autoescape())
+            template = env.get_template(f".{self.path}")
+            contents = template.render().encode()
+        
+        # Treat everything else as a static file
+        else:
+            # Get contents from file
+            with open(f"static{self.path}", "rb") as f:
+                contents = f.read()
+
+        # Serve contents
+        self.wfile.write(contents)
+    
